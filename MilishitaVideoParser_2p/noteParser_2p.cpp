519c6da594db7d21d306cb2955dbb2773fd9f05b
huhu9001 <huhu9001@163.com>
2023-11-19 20:40:56 +0800

    2p

--- MilishitaVideoParser_2p/noteParser_2p.cpp
+++ MilishitaVideoParser_2p/noteParser_2p.cpp
@@ -3,9 +3,9 @@
 unsigned const noteParser::n_channels = 2U;
 
 unsigned noteParser::GetPixelIndex(unsigned n_channel, int x_offset, int y_offset) const {
-	return (y_2pfocus - y_offset) * 3 + (x_2pfocus[n_channel] + x_offset) * linesize;
+	return (y_2pfocus - y_offset) * 3 + (x_2pfocus[n_channel] - x_offset) * linesize;
 }
 
 unsigned noteParser::GetCenterPixelIndex(int x_offset, int y_offset) const {
-	return (y_2pfocus + x_offset) * 3 + (x_2pcenter - y_offset) * linesize;
+	return (y_2pfocus - y_offset) * 3 + (x_2pcenter - x_offset) * linesize;
 }
